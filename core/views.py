33d3661c4b704f3d166ba95debc2cd8447dc353d
Kadambari Nijai <kadambarinijai2612@gmail.com>
2026-05-16 04:02:41 +0530

    Fix add about Views

--- core/views.py
+++ core/views.py
@@ -10,7 +10,8 @@ def home(request):
     return render(request, 'home.html')
 
 
-
+def about(request):
+    return render(request, 'about.html')
 
 def contact(request):
     return render(request, 'contact.html')
