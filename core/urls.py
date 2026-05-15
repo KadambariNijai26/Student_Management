44f3cf2aec95c490e0ef79d8bedccf58f92792f6
Kadambari Nijai <kadambarinijai2612@gmail.com>
2026-05-16 03:58:04 +0530

    Fix Core views and Urls

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
     path('teacher-dashboard/', views.teacher_dashboard, name='teacher_dashboard'),
 ]
--- core/views.py
+++ core/views.py
@@ -10,8 +10,10 @@ def home(request):
     return render(request, 'home.html')
 
 
-def about(request):
-    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
 
 
 def contact(request):
