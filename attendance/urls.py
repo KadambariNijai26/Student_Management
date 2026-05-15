0ba08c1e8ab5d2968789bf7d3071d28fb661af50
Kadambari Nijai <kadambarinijai2612@gmail.com>
2026-05-16 02:25:53 +0530

    Fix Urls

--- attendance/urls.py
+++ attendance/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.attendance_view, name='attendance'),
-    path('attendance/<int:id>/', views.attendance_view, name='attendance'),
+    path('attendance/<int:id>/', views.attendance_view, name='attendance_view'),
     path('update/<int:id>/', views.update_attendance, name='update_attendance'),
     path('delete/<int:id>/', views.delete_attendance, name='delete_attendance'),
     path('add-attendance/<int:id>/', views.add_attendance, name='add_attendance'),
