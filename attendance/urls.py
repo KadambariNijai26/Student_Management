56d375d221751c8d2d4e44088a50b2f8353f6a45
Kadambari Nijai <kadambarinijai2612@gmail.com>
2026-05-15 23:38:17 +0530

    Fix deployment crash (status 1 error)

--- attendance/urls.py
+++ attendance/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.attendance_view, name='attendance'),
+    path('add-attendance/', views.add_attendance),
     path('update/<int:id>/', views.update_attendance, name='update_attendance'),
     path('delete/<int:id>/', views.delete_attendance, name='delete_attendance'),
-    path('students/manage/add-attendance/', views.add_attendance)
+    
     
 ]
