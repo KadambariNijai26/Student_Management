e673d40937f0ee690a6da80acd0ef7ad2b0b5f7c
Kadambari Nijai <kadambarinijai2612@gmail.com>
2026-05-16 21:00:13 +0530

    Added UserProfile admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import UserProfile
 
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'user',
+        'role',
+        'course',
+        'phone'
+    )
+
+    search_fields = (
+        'user__username',
+        'role',
+        'course'
+    )
+
+    list_filter = (
+        'role',
+        'course'
+    )
 # Register your models here.
